ab0c0d2c670ff9609d4252362bb575dd8aa2601c
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-06-21 20:32:56 -0400

    format hour based on curtime; help on lost focus

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,23 @@
 $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
 
-// Load hour rows
+// format hour
+var fFormatCurHour = function (inHour) {
+    // receive hour of display row; return format for selected color
+    var curHour = moment().format("HH");
+    // var std24hr = moment(hour).format("HH");
+    console.log(curHour, inHour);
 // debugger;
+    if (inHour < curHour) {
+        return "past";
+    } else if (inHour == curHour) {
+        return "present";
+    };
+    return "future"
+
+};
+
+
+// Load hour rows
 for (i = 8; i < 25; i++) {
     var hourDiv = $("<div>")
         .addClass("row m-1")
@@ -24,24 +40,24 @@ for (i = 8; i < 25; i++) {
         .addClass("mt-4 text-right")
         .text(iHour + amPm);
 
-
     $("#hour-" + i).append(hourItem);
+    // debugger;
+    var rColor = fFormatCurHour(i);
 
-    var hourItem = $("<textarea>")
-        .addClass("col-10 hour-item future")
+    var hourItem = $("<p>")
+        .addClass("col-10 mb-0 hour-item")
+        .addClass(rColor)
         .text("Test Area " + i);
     $("#hour-row-" + i).append(hourItem);
 
     var hourItem = $("<div>")
         .addClass("col-1 saveBtn")
-        // ?.text("Test Area 1");
         .attr("id", "hour-btn-" + i);
 
     $("#hour-row-" + i).append(hourItem);
 
     var hourItem = $("<button>")
         .addClass("lockBtnStyle oi oi-lock-locked mt-4");
-    // ?.text("Test Area 1");
 
     $("#hour-btn-" + i).append(hourItem);
 
@@ -49,55 +65,38 @@ for (i = 8; i < 25; i++) {
 
 // task text was clicked
 $(".hour-item").on("click", function () {
-    // get current text of element
-
     var text = $(this)
         .text()
         .trim();
-    console.log(text);
-    // replace p element with a new textarea element
-    // var textInput = $("<textarea>").addClass("form-control").val(text);
     var textInput = $("<textarea>")
-        .val(text)
         .addClass("col-10")
+        .addClass("mb-0")
         .addClass("hour-item")
-        .addClass("past");
-    console.log(textInput);
-
+        .val(text)
+        .text(text);
     $(this).replaceWith(textInput);
-
-    // auto focus new element
     textInput.trigger("focus");
-    // console.log(textInput);
-    // console.log($(this).replaceWith(textInput));
 });
 
 // editable field was un-focused
-$(".hour-item").on("blur", "p", function () {
-    // get current value of textarea
+
+$(".row").on("blur", "textarea", function () {
     var text = $(this).val();
     console.log(text);
-    // get status type and position in the list
-    // var status = $(this)
-    //   .closest(".list-group")
-    //   .attr("id")
-    //   .replace("list-", "");
-    // var index = $(this)
-    //   .closest(".list-group-item")
-    //   .index();
 
     // update task in array and re-save to localstorage
     // tasks[status][index].text = text;
     // saveTasks();
 
     // recreate p element
-    var taskP = $("<span>")
-        //   .addClass("m-1")
+    var hourP = $("<p>")
         .addClass("col-10")
+        .addClass("mb-0")
         .addClass("hour-item")
-        .addClass("future")
+        .val(text)
         .text(text);
 
     // replace textarea with new content
-    $(this).replaceWith(taskP);
+    $(this).replaceWith(hourP);
+
 });

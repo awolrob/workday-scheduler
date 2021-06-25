92e5328a8774ac49270c72ed4c4280b0860c1d3e
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-06-25 00:27:59 -0400

    refactor

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,54 +1,40 @@
 /* variables */
-// Store local storage 
 // If there is nothing in hourSave set hourSave to an empty array
 var aHourSave = JSON.parse(localStorage.getItem('hourSave')) || [];
-// 8 to 18
 
 /* Listeners */
-
 // save button clicked
 $(document).on("click", ".lockBtnStyle", function () {
-    var iHourPos = $(this).attr("id");
-    saveHour($(this).attr("id"), $("#hour-p-" + iHourPos).text());
+    saveHour($(this).attr("id"), $("#hour-p-" + $(this).attr("id")).text());
 });
 
 // text to edit clicked
-$(document).on("click", ".hour-item", function () {
-    var text = $(this)
-        .text()
-        .trim();
-    var textClass = $(this).attr("class");
-    var textId = $(this).attr("id");
-
+$(document).on("click", "p.hour-item", function () {
     var textInput = $("<textarea>")
-        .attr("class", textClass)
-        .attr("id", textId)
-        .text(text);
+        .attr("class", $(this).attr("class"))
+        .attr("id", $(this).attr("id"))
+        .text($(this)
+            .text()
+            .trim()
+        );
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
 });
 
-// editable field was un-focused
+// editable field was un-focused - change back to p
 $(document).on("blur", "textarea", ".row", function () {
-    var text = $(this).val();
-    // recreate p element
-    var textClass = $(this).attr("class");
-    var textId = $(this).attr("id");
-
     var hourP = $("<p>")
-        .attr("class", textClass)
-        .attr("id", textId)
-        .text(text);
+        .attr("class", $(this).attr("class"))
+        .attr("id", $(this).attr("id"))
+        .text($(this).val());
 
-    // replace textarea with new content
     $(this).replaceWith(hourP);
 });
 /* End Listeners */
 
 /* Functions */
 
-//add load local storage
-
+//save to local storage
 var saveHour = function (hourIndex, inText) {
     aHourSave[hourIndex] = inText;
     localStorage.setItem("hourSave", JSON.stringify(aHourSave));
@@ -57,18 +43,8 @@ var saveHour = function (hourIndex, inText) {
 // format hour
 var fFormatCurHour = function (inHour) {
     // receive hour of display row; return format for selected color
-    var curHour = moment().format("HH");
-
-    //********Test Code
-    curHour = 10;
-    //*************Test Code
-
-    // var std24hr = moment(hour).format("HH");
-    if (inHour < curHour) {
-        return "past";
-    } else if (inHour == curHour) {
-        return "present";
-    };
+    if (inHour < moment().format("HH")) { return "past" };
+    if (inHour == moment().format("HH")) { return "present" };
     return "future"
 };
 /* End Functions */
@@ -77,53 +53,43 @@ var fFormatCurHour = function (inHour) {
 $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
 
 // Load hour rows
-for (i = 8; i < 18; i++) {
-    var hourDiv = $("<div>")
+for (i = 7; i < 18; i++) {
+    //<div class="row m-1" id="hour-row-8">
+    $("#hour-list").append($("<div>")
         .addClass("row m-1")
-        .attr("id", "hour-row-" + i);
-
-    $("#hour-list").append(hourDiv);
-
-    var hourCol = $("<div>")
+        .attr("id", "hour-row-" + i)
+    );
+    //<div class="col-1 hourDiv p-0 pr-1 align-middle" id="hour-div-8">
+    $("#hour-row-" + i).append($("<div>")
         .addClass("col-1")
         .attr("id", "hour-div-" + i)
-        .addClass("hourDiv p-0 pr-1 align-middle");
+        .addClass("hourDiv p-0 pr-1 align-middle")
+    );
 
-    $("#hour-row-" + i).append(hourCol);
-
-    var amPm = "am";
+    var amPm = " am";
     var iHour = i;
-    if (i > 11) { amPm = "pm" };
+    if (i > 11) { amPm = " pm" };
     if (i > 12) { iHour = i - 12 };
-    var hourItem = $("<p>")
+    //<p class="mt-4 text-right">8 am</p>
+    $("#hour-div-" + i).append($("<p>")
         .addClass("mt-4 text-right")
-        .text(iHour + " " + amPm);
-
-    $("#hour-div-" + i).append(hourItem);
-
-    var rColor = fFormatCurHour(i);
-
-    hourText = aHourSave[i];
-
-    console.log(hourText);
-
-    var hourItem = $("<p>")
+        .text(iHour + amPm)
+    );
+    //<p class="col-10 mb-0 hour-item past" id="hour-p-8">item text</p>
+    $("#hour-row-" + i).append($("<p>")
         .addClass("col-10 mb-0 hour-item")
-        .addClass(rColor)
+        .addClass(fFormatCurHour(i))
         .attr("id", "hour-p-" + i)
-        .text(hourText);
-
-    $("#hour-row-" + i).append(hourItem);
-
-    var hourItem = $("<div>")
+        .text(aHourSave[i])
+    );
+    //<div class="col-1 saveBtn" id="hour-btn-8">
+    $("#hour-row-" + i).append($("<div>")
         .addClass("col-1 saveBtn")
-        .attr("id", "hour-btn-" + i);
-
-    $("#hour-row-" + i).append(hourItem);
-
-    var hourItem = $("<button>")
+        .attr("id", "hour-btn-" + i)
+    );
+    //<button class="lockBtnStyle oi oi-lock-locked mt-4" id="8"></button>
+    $("#hour-btn-" + i).append($("<button>")
         .addClass("lockBtnStyle oi oi-lock-locked mt-4")
-        .attr("id", i);
-
-    $("#hour-btn-" + i).append(hourItem);
+        .attr("id", i)
+    );
 }
